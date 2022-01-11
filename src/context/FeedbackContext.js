0a78d429b12a84d823c1f9fd1d34cca656fbc8d7
matije11 <matije.vukovic@gmail.com>
2022-01-11 14:52:28 +0100

    added json-server api

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,26 +1,48 @@
-import { createContext, useState } from 'react';
-import { v4 as uuidv4 } from 'uuid';
-import FeedbackData from '../data/FeedbackData';
+import { createContext, useState, useEffect } from 'react';
 
 const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({ children }) => {
-    const [feedback, setFeedback] = useState(FeedbackData);
+    const [isLoading, setIsLoading] = useState(true);
+    const [feedback, setFeedback] = useState([]);
     const [feedbackEdit, setFeedbackEdit] = useState({
         item: {},
         edit: false
     });
 
+    useEffect(() => {
+        fetchFeedback();
+    }, [])
+
+    // fetch feedback
+    const fetchFeedback = async () => {
+        const response = await fetch('/feedback?_sort=id&_order=desc');
+        const data = await response.json();
+        setFeedback(data);
+        setIsLoading(false);
+    }
+
     // f-ja za brisanje feedback zapisa
-    const deleteFeedback = (id) => {
+    const deleteFeedback = async (id) => {
         const newData = feedback.filter(item => item.id !== id);
+        await fetch(`/feedback/${id}`, {
+            method: 'DELETE'
+        })
         setFeedback(newData);
     }
+
     // f-ja za dodavanje feedback zapisa
-    const addFeedback = (newFeedback) => {
-        newFeedback.id = uuidv4(); // dodavanje random id-a
-        setFeedback([newFeedback, ...feedback]);
+    const addFeedback = async (newFeedback) => {
+        const response = await fetch('/feedback', {
+            method: "POST",
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(newFeedback)
+        })
+        const data = await response.json();
+        //newFeedback.id = uuidv4(); // dodavanje random id-a
+        setFeedback([data, ...feedback]);
     }
+
     // f-ja za setovanje statusa za izmenu feedbacka
     const editFeedback = (item) => {
         setFeedbackEdit({
@@ -28,12 +50,18 @@ export const FeedbackProvider = ({ children }) => {
             edit: true
         })
     }
-    // f-ja za izmenu feedback-a
-    const updateFeedback = (id, updatedItem) => {
-        setFeedback(feedback.map((item) => item.id === id ?
-            { ...item, ...updatedItem } : item));
-    }
 
+    // f-ja za izmenu feedback-a
+    const updateFeedback = async (id, updatedItem) => {
+        const response = await fetch(`/feedback/${id}`, {
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(updatedItem)
+        })
+        const data = await response.json();
+        setFeedback(feedback.map((item) => item.id === id ?
+            { ...item, ...data } : item));
+    }
 
     return <FeedbackContext.Provider value={{
         feedback,
@@ -41,7 +69,8 @@ export const FeedbackProvider = ({ children }) => {
         addFeedback,
         feedbackEdit, // state
         editFeedback, //f-ja
-        updateFeedback
+        updateFeedback,
+        isLoading
     }}>
         {children}
     </FeedbackContext.Provider>
